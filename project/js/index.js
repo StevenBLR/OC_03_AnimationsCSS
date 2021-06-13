e279c4b3ab6639ccb79f1cf6ace43f98fc81a3d6
@StevenBLR <s.belair95@gmail.com>
2021-06-13 22:45:56 +0200

    Deleting Js + Z-index trick

--- project/js/index.js
+++
@@ -1,9 +0,0 @@
-const loadingElt = document.querySelector(".loading");
-
-function HideElt(elt, delay){
-    setTimeout(function(){
-        elt.style.display = "none";
-    }, delay);
-}
-
-HideElt(loadingElt,7000); // Hide loading UI after 7 seconds
